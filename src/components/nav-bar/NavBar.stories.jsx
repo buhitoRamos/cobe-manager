7016bc7672f0a6cf40d8bf575b6c74ef7e6096b6
Martin Lopez <martin.lopez3@bbva.com>
2023-10-07 18:40:53 -0300

    feat(*): add update login page and add new component

---
+++ src/components/nav-bar/NavBar.stories.jsx
@@ -0,0 +1,10 @@
+// NavBar.stories.js
+import React from 'react';
+import NavBar from './NavBar';
+
+export default {
+  title: 'NavBar',
+  component: NavBar,
+};
+
+export const Primary = () => <NavBar color="primary" />;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,12 +1,29 @@
 import React from "react";
+import "./login.css";
+import guitarra from "../img/guitarra.svg";
+
+
 
 
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login page</h1>
-    </div>
+    <section id="login" class="section-container">
+        <header class="header-class">
+            <h1>Bienvenido a cobe manager</h1>
+            <h3>Por favor ingrese sus credenciales</h3>
+        </header>
+        <div>
+            <form class="form-container">
+            <img src={guitarra} alt="logo" class="img-class"/>
+                <label>Usuario</label>
+                <input type="text" name="user" />
+                <label>Contraseña</label>
+                <input type="password" name="contraseña" />
+                <input class="button-submit" type="submit" value="ingresar" />
+            </form>
+        </div>
+    </section>
   );
 }  
 export default Login;
