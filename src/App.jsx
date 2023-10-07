e611e29254da4b28b183c7b4bef5433a3185cacf
Martin Lopez <martin.lopez3@bbva.com>
2023-10-07 16:51:28 -0300

    feat(*): add a login page and update git ignore

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "./pages/login";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+    <Switch>
+        <Route exact path="/">
+           <Login />
+        </Route>
+    </Switch>
+</Router>
+  );
+}
+
+export default App;
